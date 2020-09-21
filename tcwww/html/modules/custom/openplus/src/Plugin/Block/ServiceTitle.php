c477d423dbc5538793079db54d8cd6ee233513dc
Joel Brockbank <joelb@Joels-MacBook-Pro.local>
2020-09-21 16:10:16 -0400

    Adding new blocks for services

---
+++ tcwww/html/modules/custom/openplus/src/Plugin/Block/ServiceTitle.php
@@ -0,0 +1,49 @@
+<?php
+  
+namespace Drupal\openplus\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Datetime\DateFormatter;
+use Drupal\Core\Datetime\Entity\DateFormat;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+/**
+ * Provides the title for a service with a service block (entityqueue).
+ *
+ * @Block(
+ *   id = "op_service_title",
+ *   admin_label = @Translation("Service title"),
+ *   category = @Translation("Openplus"),
+ *   context = {
+ *     "node" = @ContextDefinition("entity:node")
+ *   }
+ * )
+ */
+class ServiceTitle extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $node = $this->getContextValue('node');
+    $build = [];
+
+    // Node context.
+    if (is_object($node)) {
+      if ($node->hasField('field_service_block') && !$node->get('field_service_block')->isEmpty()) {
+        $serviceblock = $node->get('field_service_block')->getValue();
+        $entityqueue = \Drupal::entityTypeManager()->getStorage('entity_subqueue')->load($serviceblock[0]['target_id']);
+        if ($entityqueue) {
+          $build['service_title']['#markup'] = '<div class="h4">' . $entityqueue->getTitle() . '</div>';
+        }
+      }
+    }
+
+    return $build;
+  }
+
+}
